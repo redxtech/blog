37da94686d47b5a2c391ada223cbb98f0cf4d68d
Gabe Dunn <gabe@sent.at>
2023-03-09 11:11:49 -0800

    change blockquote style and rename post format

--- .vitepress/theme/Article.vue
+++ .vitepress/theme/Article.vue
@@ -23,46 +23,39 @@ const prevPost = computed(() => posts[findCurrentIndex() + 1])
   <article class="xl:divide-y xl:divide-gray-200 dark:xl:divide-slate-200/5">
     <header class="pt-6 xl:pb-10 space-y-1 text-center">
       <Date :date="date" />
-      <h1
-        class="
-          text-3xl
-          leading-9
-          font-bold
-          text-gray-900 dark:text-white
-          tracking-tight
-          sm:text-4xl sm:leading-10
-          md:text-5xl md:leading-14
-        "
-      >
+      <h1 class="
+            text-3xl
+            leading-9
+            font-bold
+            text-gray-900 dark:text-white
+            tracking-tight
+            sm:text-4xl sm:leading-10
+            md:text-5xl md:leading-14
+          ">
         {{ data.title }}
       </h1>
     </header>
 
-    <div
-      class="
-        divide-y
-        xl:divide-y-0
-        divide-gray-200 dark:divide-slate-200/5
-        xl:grid xl:grid-cols-4 xl:gap-x-10
-        pb-16
-        xl:pb-20
-      "
-      style="grid-template-rows: auto 1fr"
-    >
+    <div class="
+          divide-y
+          xl:divide-y-0
+          divide-gray-200 dark:divide-slate-200/5
+          xl:grid xl:grid-cols-4 xl:gap-x-10
+          pb-16
+          xl:pb-20
+        " style="grid-template-rows: auto 1fr">
       <Author />
       <div class="divide-y divide-gray-200 dark:divide-slate-200/5 xl:pb-0 xl:col-span-3 xl:row-span-2">
         <Content class="prose dark:prose-invert max-w-none pt-10 pb-8" />
       </div>
 
-      <footer
-        class="
-          text-sm
-          font-medium
-          leading-5
-          divide-y divide-gray-200 dark:divide-slate-200/5
-          xl:col-start-1 xl:row-start-2
-        "
-      >
+      <footer class="
+            text-sm
+            font-medium
+            leading-5
+            divide-y divide-gray-200 dark:divide-slate-200/5
+            xl:col-start-1 xl:row-start-2
+          ">
         <div v-if="nextPost" class="py-8">
           <h2 class="text-xs tracking-wide text-gray-500 dark:text-white">
             next article
@@ -86,3 +79,13 @@ const prevPost = computed(() => posts[findCurrentIndex() + 1])
     </div>
   </article>
 </template>
+
+<style>
+.prose blockquote>p::before {
+  content: none;
+}
+
+.prose blockquote>p::after {
+  content: none;
+}
+</style>
