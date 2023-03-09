ba3f72341572a24f63d19040ac98d18464451feb
Gabe Dunn <gabe@sent.at>
2023-03-09 12:02:34 -0800

    change code block styling

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: 'gabe\'s blog',
-  description: 'where i talk about linux, programming, and other fun computery stuff',
-	// TODO add opengraph metadata
-	// TODO change all metadata
+  title: "gabe's blog",
+  description:
+    "where i talk about linux, programming, and other fun computery stuff",
+  // TODO add opengraph metadata
+  // TODO change all metadata
   head: [
-    ['meta', { name: 'twitter:site', content: '@gabedunn_' }],
-    ['meta', { name: 'twitter:card', content: 'summary' }],
+    ["meta", { name: "twitter:site", content: "@gabedunn_" }],
+    ["meta", { name: "twitter:card", content: "summary" }],
     [
-      'meta',
+      "meta",
       {
-        name: 'twitter:image',
-        content: 'https://blog.gabedunn.dev/logo.png'
-      }
+        name: "twitter:image",
+        content: "https://blog.gabedunn.dev/logo.png",
+      },
     ],
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      }
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
     ],
-		// TODO add own tracking script
+    // TODO add own tracking script
     // [
     //   'script',
     //   {
@@ -33,5 +34,8 @@ export default defineConfig({
     //     defer: ''
     //   }
     // ]
-  ]
-})
+  ],
+  markdown: {
+    theme: "dracula"
+  },
+});
